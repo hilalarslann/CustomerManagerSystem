e31605577c69af87d9d36d098b253eabb593be46
hilalarslann <hilal.fbk@gmail.com>
2022-06-24 14:05:42 +0300

    Add project files.

---
+++ CustomerManagerSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomerManagerSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
